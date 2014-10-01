5ecbe26669d295a2a1b032e3e42bdb58010b0af7
SUGINO Hironori <komenari@gmail.com>
2014-10-01 20:25:27 +0900

    Create Hana1

---
+++ Assets/Hana/Scripts/Hana1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hana1 : MonoBehaviour
+{
+	Vector3 GetHanaPos ()
+	{
+		Vector3 pos = Random.insideUnitSphere;
+		pos.x *= 2f;
+		pos.y *= 10f;
+		pos.z *= 2f;
+		pos.y += 5f;
+		Quaternion rot = Quaternion.Euler (Vector3.forward * Random.Range (0, 5) * 360f / 5f);
+		return rot * pos;
+	}
+
+	void Start ()
+	{
+		Light l = new GameObject ("light").AddComponent<Light> ();
+		l.type = LightType.Directional;
+		l.transform.LookAt (Random.insideUnitSphere + Vector3.forward);
+
+		Camera.main.backgroundColor = Color.cyan;
+		Camera.main.transform.position = -Vector3.forward * 25f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GameObject go = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		go.transform.position = GetHanaPos ();
+		go.AddComponent<Rigidbody> ();
+		go.rigidbody.drag = 10f;
+		go.renderer.material.color = Color.Lerp (Color.yellow, Color.magenta, go.transform.position.sqrMagnitude * 0.06f);
+		Destroy (go, 5f);
+	}
+}
